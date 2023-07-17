14c017d0ead108872d641dc00e87b85faae34d26
wkice-wilker <92658448+wkice-wilker@users.noreply.github.com>
2023-07-17 15:30:50 -0300

    criando movit

---
+++ moveit/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './Componentes/Button'
+
+function App() {
+  return (
+   <div>
+      <h1> ola Wkice</h1>
+
+      <Button  color="#ffff"> teste </Button>
+      <Button  color="orange"> teste </Button>
+
+    </div>
+  );
+}
+
+export default App;
